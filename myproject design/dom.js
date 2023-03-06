e22cd372d1d9147249d9284a9dfa5631b2dda90b
shivam5676 <ssultanpura9@gmail.com>
2023-03-06 15:14:54 -0800

    last sumit using tag

--- myproject design/dom.js
+++ myproject design/dom.js
@@ -1,5 +1,6 @@
-var items=document.getElementsByClassName('list-group-item');
-console.log(items[1]);
-items[1].textContent='hello';
-for(var i=0;i<items.length;i++){
-items[i].style.fontWeight="bold";}
+var li=document.getElementsByTagName('li');
+console.log(li);
+for(var i= 0 ;i<li.length; i++)
+{
+    li[i].style.backgroundColor="grey"
+}
